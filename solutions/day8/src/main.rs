0028c724bfaf6e1d85b968e8042070f464c93107
razzdrgn <razzdrgn@protonmail.com>
2023-12-10 19:41:56 -0500

    added comments explaining problems/fixes and did some mild formatting

--- solutions/day8/src/main.rs
+++ solutions/day8/src/main.rs
@@ -18,19 +18,23 @@ use serde_json::value::Value;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new()
-        .route("/8/weight/:pokedex_number", get(pokemon_weight));
+	let router = Router::new().route("/8/weight/:pokedex_number", get(pokemon_weight));
 
-    async fn pokemon_weight(Path(pokedex_number): Path<i64>) -> String {
-        println!("{:?}", pokedex_number);
+	async fn pokemon_weight(Path(pokedex_number): Path<i64>) -> String {
+		println!("{:?}", pokedex_number);
 
-        let pokeapi: String = format!(
-            "https://pokeapi.co/api/v2/pokemon/{0}", pokedex_number
-        );
+		let pokeapi: String = format!(
+			"https://pokeapi.co/api/v2/pokemon/{}", // Format string doesn't need any indices, so the 0 is unecessary
+			pokedex_number
+		);
 
-        reqwest::get(&pokeapi).await.expect("req couldnt finish");
-        pokedex_number.to_string()
-    }
+		let resp = reqwest::get(&pokeapi).await.expect("req couldnt finish"); // Store the response as an object here
 
-    Ok(router.into())
+		// pokedex_number.to_string() This just outputs the dex number to string, which does not do anything productive for us.
+		// Put your new processing logic here in order to get the data out of the reqwest::Response object
+
+		"".to_string()
+	}
+
+	Ok(router.into())
 }
